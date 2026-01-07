5ce943a45c8859dcfb8ec8627bdec7b39d3e31a2
7853186-oss <7853186@stud.kai.edu.ua>
2026-01-07 15:25:23 +0200

    Add files via upload

---
+++ EchoTcpServer/UdpTimedSender.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace EchoTcpServerApp.Client
+{
+    public class UdpTimedSender : IDisposable
+    {
+        private readonly string _host;
+        private readonly int _port;
+        private readonly UdpClient _udpClient;
+        private Timer? _timer; // Nullable
+
+        public UdpTimedSender(string host, int port)
+        {
+            _host = host;
+            _port = port;
+            _udpClient = new UdpClient();
+        }
+
+        public void StartSending(int intervalMilliseconds)
+        {
+            if (_timer != null)
+                throw new InvalidOperationException("Sender is already running.");
+
+            _timer = new Timer(SendMessageCallback, null, 0, intervalMilliseconds);
+        }
+
+        ushort _counter = 0;
+
+        // Smell fix: state is nullable
+        private void SendMessageCallback(object? state)
+        {
+            try
+            {
+                byte[] samples = new byte[1024];
+                Random.Shared.NextBytes(samples);
+                _counter++;
+
+                byte[] msg = (new byte[] { 0x04, 0x84 })
+                             .Concat(BitConverter.GetBytes(_counter))
+                             .Concat(samples).ToArray();
+                             
+                var endpoint = new IPEndPoint(IPAddress.Parse(_host), _port);
+
+                _udpClient.Send(msg, msg.Length, endpoint);
+                Console.WriteLine($"Message sent to {_host}:{_port} ");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error sending message: {ex.Message}");
+            }
+        }
+
+        public void StopSending()
+        {
+            _timer?.Dispose();
+            _timer = null;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                StopSending();
+                _udpClient.Dispose();
+            }
+        }
+    }
+}
